f44e635084386f4bd2722866aaa1453c1e74fbbe
Jayanti Upadhyay <updjayanti@gmail.com>
2021-01-17 08:56:52 -0500

    some more updates

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import Dashboard from "./components/dashboard";
+import Login from "./components/login";
+import Register from "./register/register";
+import Routes from "./components/routes"
+
+import { Provider } from "react-redux";
+import store from "./store";
+import { loadUser } from "./actions/auth";
+import setAuthToken from "./utils/setAuthToken";
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
 function App() {
+
+  const unmount = () => {
+    var root = document.getElementById("overlay");
+    root.parentNode.removeChild(root)
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
---
+++ src/components/register/index.js
@@ -0,0 +1,100 @@
+
+import React, { Fragment, useState } from "react";
+import { connect } from "react-redux";
+import { register } from "../../../authtingsidk/auth";
+import { Link, Redirect } from "react-router-dom";
+import PropTypes from "prop-types";
+
+const Register = ({ setAlert, register, isAuthenticated }) => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (password !== password2) {
+      alert("Passwords do not match", "danger");
+    } else {
+      register({ name, email, password });
+      console.log("Register Component success");
+    }
+  };
+
+  if (isAuthenticated) {
+    //Redirect to dashboard if logged in
+    return <Redirect to="dashboard" />;
+  }
+  return (
+    <Fragment>
+      <h1 className="large text-primary">Sign Up</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Create Your Account
+      </p>
+      <form className="form" onSubmit={(e) => onSubmit(e)}>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={name}
+            onChange={(e) => onChange(e)}
+            // required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={(e) => onChange(e)}
+            // required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            minLength="4"
+            value={password}
+            onChange={(e) => onChange(e)}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="password2"
+            minLength="4"
+            value={password2}
+            onChange={(e) => onChange(e)}
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Register" />
+      </form>
+      <p className="my-1">
+        Already have an account? <Link to="/login">Sign In</Link>
+      </p>
+    </Fragment>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+});
+
+Register.propTypes = {
+  register: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool,
+};
+
+export default connect(mapStateToProps, { register })(Register);
